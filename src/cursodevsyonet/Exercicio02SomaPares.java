33ee135aeb58acec79e92bdebb91509a78f13239
augustostein <augustostein.9@gmail.com>
2022-07-11 22:27:24 -0300

    exercicio aula

--- src/cursodevsyonet/Exercicio02SomaPares.java
+++ src/cursodevsyonet/Exercicio02SomaPares.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Exercicio02SomaPares {
     public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um número");
+        int total = teclado.nextInt();
         int soma = 0;
-        for (int i = 0; i <= 20; i++) {
+
+        for (int i = 0; i <= total; i++) {
             if (i % 2 == 0) {
                 soma = soma + i;
             }
         }
-        System.out.println(soma);
+        System.out.println("A soma total dos números pares é: " + soma);
     }
 }
